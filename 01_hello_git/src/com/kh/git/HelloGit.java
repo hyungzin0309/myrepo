9571f496ba9c5cc1cbae808a55b12fb0dbc59dd6
hyungzin0309 <hyungzin0309@naver.com>
2021-10-25 20:08:36 +0900

    23333334

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -8,9 +8,8 @@ public class HelloGit {
 		System.out.println("hello master~");
 	}
 	public void merge() {
-		System.out.println(1);
 		System.out.println(2);
-		System.out.println(3);
+		System.out.println(333333);
 		System.out.println(4);
 	}
 }
